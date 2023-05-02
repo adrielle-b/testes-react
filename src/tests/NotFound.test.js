c39b7d3090f86aee0d09b765d8caac9549a54de8
adrielle-b <adriielle@gmail.com>
2023-05-02 18:20:04 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes do componente Not Found', () => {
+  test('Teste se a página contém um heading h2 com o texto "Page requested not found"', () => {
+    const { history } = renderWithRouter(<App />);
+    const invalidaUrl = '/xablau';
+    act(() => {
+      history.push(invalidaUrl);
+    });
+    screen.getByRole('heading', { name: /page requested not found/i });
+  });
+  test('Teste se a página mostra a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+    const invalidaUrl = '/xablau';
+    act(() => {
+      history.push(invalidaUrl);
+    });
+    const elementImg = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    const imagemUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(elementImg).toHaveAttribute('src', imagemUrl);
+  });
+});
